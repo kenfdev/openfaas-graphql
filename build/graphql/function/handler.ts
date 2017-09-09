fecf26cc9ea544219b6b8f368e30fd33e43ebb1a
Ken Fukuyama <kenfdev@gmail.com>
2017-09-10 01:57:57 +0900

    rebuild

--- build/graphql/function/handler.ts
+++ build/graphql/function/handler.ts
@@ -4,27 +4,13 @@ import {
     execute
 } from 'graphql';
 import { StarWarsSchema } from './data/swapiSchema';
-import { GraphQLOptions, runHttpQuery } from 'apollo-server-core';
 
 module.exports = (context: any, callback: any) => {
 
     const body = JSON.parse(context);
 
-    const options: GraphQLOptions = {
-        schema: StarWarsSchema,
-    }
-
-    runHttpQuery([], {
-        method: 'POST',
-        options: options,
-        query: body.query,
-    }).then(result => {
+    graphql(StarWarsSchema, body.query).then(result => {
         callback(undefined, result);
     }).catch(err => callback(err, undefined));
-
-
-    // graphql(StarWarsSchema, body.query).then(result => {
-    //     callback(undefined, result);
-    // }).catch(err => callback(err, undefined));
 
 }
